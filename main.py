51107c162602a8bb05c65c09e3fa2147c9b435d0
E.I.Moskvin <evgeni.moskvin@gmail.com>
2024-05-23 08:28:06 +0300

    upd

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from ui.icon import rc_icon
 
 
 def get_pages(path):
+    # Стандартизированные размеры листов по ГОСТ
     page_size_standard = {
         'A0': (841, 1189, 16),
         'A0x2': (1189, 1682, 32),
@@ -34,7 +35,7 @@ def get_pages(path):
         'A4x8': (297, 1682, 8),
         'A4x9': (297, 1892, 9),
     }
-
+    # Пустой словарь для подсчета листов
     pdf_size_file = {
         'A0': 0,
         'A0x2': 0,
@@ -62,11 +63,9 @@ def get_pages(path):
         'A4x9': 0,
     }
 
-    # pdf_size_file = {}
-    a4_count = 0
-    normal_pages = 0
-    # path = str(input("Введите путь к файлу: "))
-    list_pages = []
+    a4_count = 0  # Счетчик форматов А4
+    normal_pages = 0  # Счетчик распознанных листов
+    list_pages = []  # Список листов
     with open(path, 'rb') as pdf_file:
         pdf_reader = PyPDF2.PdfReader(pdf_file)
         i = 0
@@ -74,15 +73,18 @@ def get_pages(path):
         for i in range(num_pages):
             checker = 0
             box = pdf_reader.pages[i]
+            # Переводим значения в миллиметры
             width = math.ceil(float(box.mediabox.width) * 0.35273159)
             height = math.ceil(float(box.mediabox.height) * 0.35273159)
             list_pages.append([height, width])
+            # Приводим к одной ориентации
             if width < height:
                 small = width
                 long = height
             else:
                 small = height
                 long = width
+            # Проверяем вхождение данных размеров в стандартные, с допуском 15мм
             for key, value in page_size_standard.items():
                 if (value[0] >= small - 15) and (value[0] <= small + 15) and (value[1] <= long + 15) and (
                         value[1] >= long - 15):
@@ -90,16 +92,19 @@ def get_pages(path):
                     a4_count += value[2]
                     pdf_size_file[key] += 1
                     checker = 1
+            # Если в стандартные значения не вошли
             if checker == 0:
                 size = f'{small}x{long}'
                 try:
-                    pdf_size_file[size] += 1
+                    pdf_size_file[size] += 1  # Если такой размер уже есть в словаре
                 except:
-                    pdf_size_file[size] = 1
+                    pdf_size_file[size] = 1  # Если такого размера нет в словаре
+    # Переносим все пустые значения в список
     temp_list = []
     for key, value in pdf_size_file.items():
         if value == 0:
             temp_list.append(key)
+    # Удаляем из словаря все пустые значения
     for i in temp_list:
         del pdf_size_file[i]
 
@@ -108,8 +113,6 @@ def get_pages(path):
     print(f'Всего страниц: {num_pages}')
     print(f'Всего распознано страниц: {normal_pages}')
     return pdf_size_file, a4_count, num_pages, normal_pages, list_pages
-    # C:\Users\E.I.Moskvin\Downloads\sp_131.13330.2020_stroitelnaya_klimatologiya(1).pdf
-    #  C:\Users\E.I.Moskvin\Downloads\4293851086.pdf
 
     # pyside2-uic "you_file.ui" -o "your_file.py"
 
@@ -117,6 +120,9 @@ def get_pages(path):
 class TableModel(QtCore.QAbstractTableModel):
 
     def headerData(self, section: int, orientation: QtCore.Qt.Orientation, role: int = ...):
+        """
+        Устновка значений шапки таблицы
+        """
         if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
             # return f"Column {section + 1}"
             return self.columns[section]
@@ -126,7 +132,6 @@ class TableModel(QtCore.QAbstractTableModel):
     def __init__(self, data):
         self.columns = ["Формат", "Количество"]
         super(TableModel, self).__init__()
-
         self._data = data
 
     def data(self, index, role):
@@ -145,6 +150,8 @@ class TableModel(QtCore.QAbstractTableModel):
 
 
 class TableModel2(TableModel):
+    """Вторая таблица"""
+
     def __init__(self, data):
         self.columns = ["Высота", "Ширина"]
         super(TableModel, self).__init__()
@@ -155,32 +162,37 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        rc_icon.qInitResources()
-        self.setWindowIcon(QtGui.QIcon(QtGui.QPixmap(":/ico/Calculator_30001.ico")))
+        rc_icon.qInitResources()  # Подгружаем файл с иконками
+        self.setWindowIcon(QtGui.QIcon(QtGui.QPixmap(":/ico/Calculator_30001.ico")))  # Устанаваливаем иконки
         # resource.qInitResources()
-        self.ui = Ui_MainWindow()
+        self.ui = Ui_MainWindow()  # Подключаем Ui
         self.ui.setupUi(self)
         self.file_list = []
         self.data_formats = []
-        self.setAcceptDrops(True)
+        self.setAcceptDrops(True)  # Подключаем drag and drop
 
         # Сигналы
-        self.ui.open_pdf_button.clicked.connect(self.add_files_to_check)
+        self.ui.open_pdf_button.clicked.connect(self.add_files_to_check)  # Кнопка открытия pdf
 
     def check_pdf_file(self, fname):
+        """
+        Функция вызова подсчета листов в файле, get_pages
+        :param fname: строка с адресом к файла, str
+        """
         self.file_list.append(fname)
-        print(fname, type(fname))
+        print(fname, type(fname))  # Полный путь к файлу
         try:
             self.file_name = fname.split('/')[-1]
         except:
             self.file_name = fname
-        print(self.file_name)
+        print(self.file_name)  # Наименование файла
         pdf_size_file, a4_count, num_pages, normal_pages, list_pages = get_pages(fname)
         self.ui.a4_count_text.setText(str(a4_count))
         self.ui.num_pages_text.setText(str(num_pages))
         self.ui.normal_text.setText(str(normal_pages))
         self.ui.fileName.setText(str(self.file_name))
         self.data_formats = []
+        # Делаем список, для подстановки в таблицу
         for key, value in pdf_size_file.items():
             self.data_formats.append([key, value])
             print(key, value)
@@ -188,8 +200,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.data_pages = list_pages
         self.model_pages = TableModel2(self.data_pages)
 
-        self.ui.tableView.setModel(self.model_formats)
-        self.ui.tableView_2.setModel(self.model_pages)
+        self.ui.tableView.setModel(self.model_formats)  # Устанавливаем значения в таблицу левую
+        self.ui.tableView_2.setModel(self.model_pages)  # Устанавливаем значения в таблицу правую
 
     def dragEnterEvent(self, event):
         mime = event.mimeData()
@@ -203,6 +215,7 @@ class MainWindow(QtWidgets.QMainWindow):
         return super().dropEvent(event)
 
     def add_files_to_check(self):
+        """Функция при нажатии кнопки открыть pdf"""
         fname = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file', "*.pdf")[0]
         self.check_pdf_file(fname)
 
